40e172586d1ffef2120a4594efc1d7a318289ff4
hhhuang <hhhuang@nccu-demo.c.ntu-nlp-03734301.internal>
2024-03-22 16:56:12 +0000

    adding setup.py

--- setup.py
+++ setup.py
@@ -1,30 +1,41 @@
-import os
-import sys
-from setuptools import find_packages, setup
-
-def read(rel_path: str) -> str:
-    here = os.path.abspath(os.path.dirname(__file__))
-    # intentionally *not* adding an encoding option to open, See:
-    #   https://github.com/pypa/virtualenv/issues/201#issuecomment-3145690
-    with open(os.path.join(here, rel_path)) as fp:
-        return fp.read()
-
-def get_version(rel_path: str) -> str:
-    for line in read(rel_path).splitlines():
-        if line.startswith("__version__"):
-            # __version__ = "0.9"
-            delim = '"' if '"' in line else "'"
-            return line.split(delim)[1]
-    raise RuntimeError("Unable to find version string.")
-
+from setuptools import setup, find_packages
 
 setup(
-    version=get_version("__init__.py"),
-    package_dir={"": "ChineseWordSegmenter"},
-    packages=find_packages(
-        where=".",
-        exclude=[],
-    ),
+    name='TraditionalChineseWordSegmenter',
+    version='0.1',
+    packages=find_packages(),
+    license='MIT License',
+    description='A BERT-based Chinese Word Segmentation Model Specific to Traditional Chinese (zh_TW)',
+    long_description=open('README.md').read(),
+    install_requires=[
+        "transformers", 
+        "simpletransformers",
+        "pandas",
+        "requests",
+        # List all project dependencies here.
+        # For example: 'requests >= 2.19.1'
+    ],
+    url='https://github.com/hhhuang/ChineseWordSegmenter',
+    author='Hen-Hsen Huang',
+    author_email='hhhuang@iis.sinica.edu.tw',
+    classifiers=[
+        # Classifiers help users find your project by categorizing it.
+        
+        # For a list of valid classifiers, see https://pypi.org/classifiers/
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+    keywords='Chinese NLP word segmentation tokenize',  # Keywords that define your package best
+    entry_points={
+        'console_scripts': [
+            # Entry points create executable commands by pointing
+            # to a function in your package.
+            # For example: 'your_command = your_package.module:function'
+        ],
+    },
+    # Include package data files from MANIFEST.in
     include_package_data=False,
-    zip_safe=False,
+    # Other arguments here...
 )
